96f618475fa14959851fa570716b7f59a47eda4f
FazliddinAbdazimov <abdazimovfazliddin27@gmail.com>
2025-05-16 16:12:48 +0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Fazliddin Abdazimov on 16/05/25.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
